3330d67ebb734097f2e63aac786a4c3272ac0d86
Praghav49 <raghavpankaj381@gmail.com>
2023-09-08 23:07:36 +0530

    first commit

---
+++ funtion.py
@@ -0,0 +1,35 @@
+def calculategmean(a, b):
+  mean = (a*b)/(a+b)
+  print(mean)
+
+def isgreater(a, b):
+  if(a>b):
+    print("first no. is greater")
+  else:
+    print("second no. is greater oe equal")
+
+def islesser(a, b):
+  if(a<b):
+    print("frist no. is lesser")
+  else:
+    print("second no. is lesser or equal")
+
+a = 9
+b = 8
+isgreater(a, b)
+calculategmean(a, b)
+islesser(a, b)
+
+c = 8
+d = 7
+isgreater(c, d)
+calculategmean(c, d)
+islesser(c, d)
+
+e = 4
+f = 2
+isgreater(e, f)
+calculategmean(e, f)
+islesser(e, f)
+
+
